f74edd4ec493cd5a67c8044c61f16db8feaf78ab
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2021-01-31 20:35:52 -0800

    Shortened arguments, fleshed out docs for multiModalMNN.

--- tests/testthat/test-multi-mnn.R
+++ tests/testthat/test-multi-mnn.R
@@ -16,7 +16,7 @@ BSPARAM <- BiocSingular::ExactParam()
 test_that("multiModalMNN recapitulates fastMNN", {
     batch <- sample(1:3, ncol(exprs_sce), replace=TRUE)
 
-    corrected <- multiModalMNN(exprs_sce, batch=batch, which=character(0), pca.common.args=list(BSPARAM=BSPARAM))
+    corrected <- multiModalMNN(exprs_sce, batch=batch, which=character(0), common.args=list(BSPARAM=BSPARAM))
     ref <- batchelor::fastMNN(exprs_sce, batch=batch, BSPARAM=BSPARAM)
 
     ratio <- reducedDim(corrected)/reducedDim(ref)
@@ -26,7 +26,7 @@ test_that("multiModalMNN recapitulates fastMNN", {
     # Trying with multiple experiments.
     copy <- exprs_sce
     altExp(copy) <- removeAltExps(exprs_sce)
-    corrected2 <- multiModalMNN(copy, batch=batch, which="Spikes", pca.common.args=list(BSPARAM=BSPARAM))
+    corrected2 <- multiModalMNN(copy, batch=batch, which="Spikes", common.args=list(BSPARAM=BSPARAM))
 
     expect_equal(reducedDim(corrected), reducedDim(corrected2)[,1:50])
     expect_equal(reducedDim(corrected), reducedDim(corrected2)[,50+1:50])
@@ -36,7 +36,7 @@ test_that("multiModalMNN recapitulates fastMNN", {
     # Trying with experiments split across multiple objects.
     corrected3 <- multiModalMNN(
         exprs_sce[,batch==1], exprs_sce[,batch==2], exprs_sce[,batch==3],
-        which=character(0), pca.common.args=list(BSPARAM=BSPARAM)
+        which=character(0), common.args=list(BSPARAM=BSPARAM)
     )
 
     ref <- reducedDim(corrected)[order(batch),]
@@ -48,7 +48,7 @@ test_that("multiModalMNN recapitulates fastMNN", {
     # And again, with multiple objects in multiple experiments.
     corrected4 <- multiModalMNN(
         copy[,batch==1], copy[,batch==2], copy[,batch==3],
-        which="Spikes", pca.common.args=list(BSPARAM=BSPARAM)
+        which="Spikes", common.args=list(BSPARAM=BSPARAM)
     )
 
     ref <- reducedDim(corrected2)[order(batch),]
@@ -62,12 +62,12 @@ test_that("multiModalMNN rescales correctly", {
     batch <- sample(1:3, ncol(exprs_sce), replace=TRUE)
 
     suppressWarnings(ref <- multiModalMNN(exprs_sce, batch=batch, which="ADT",
-        pca.common.args=list(BSPARAM=BSPARAM)))
+        common.args=list(BSPARAM=BSPARAM)))
 
     copy <- exprs_sce
     logcounts(altExp(copy,2 )) <- 10*logcounts(altExp(copy, 2))
     suppressWarnings(alt <- multiModalMNN(copy, batch=batch, which="ADT",
-        pca.common.args=list(BSPARAM=BSPARAM)))
+        common.args=list(BSPARAM=BSPARAM)))
 
     expect_equal(alt, ref)
 })
@@ -76,21 +76,21 @@ test_that("multiModalMNN respects Experiment-specific arguments", {
     batch <- sample(1:3, ncol(exprs_sce), replace=TRUE)
 
     corrected <- multiModalMNN(exprs_sce, batch=batch, which="ADT",
-        pca.common.args=list(BSPARAM=BSPARAM), pca.main.args=list(subset.row=1:100), 
-        pca.alt.args=list(ADT=list(d=NA)))
+        common.args=list(BSPARAM=BSPARAM), main.args=list(subset.row=1:100), 
+        alt.args=list(ADT=list(d=NA)))
 
     ref <- multiModalMNN(exprs_sce[1:100,], batch=batch, which="ADT",
-        pca.common.args=list(BSPARAM=BSPARAM), 
-        pca.alt.args=list(ADT=list(d=NA)))
+        common.args=list(BSPARAM=BSPARAM), 
+        alt.args=list(ADT=list(d=NA)))
 
     expect_identical(ref, corrected)
     expect_identical(as.matrix(assay(altExp(ref))), tail(t(reducedDim(ref)), 20)) # expression values directly used in reduced dims.
 
     # Smart enough to correct all.
     corrected2 <- multiModalMNN(exprs_sce, batch=batch, which="ADT",
-        pca.common.args=list(BSPARAM=BSPARAM), 
-        pca.main.args=list(subset.row=1:100, correct.all=TRUE), 
-        pca.alt.args=list(ADT=list(d=NA)))
+        common.args=list(BSPARAM=BSPARAM), 
+        main.args=list(subset.row=1:100, correct.all=TRUE), 
+        alt.args=list(ADT=list(d=NA)))
 
     expect_identical(reducedDim(corrected), reducedDim(corrected2))
     expect_identical(assay(corrected), assay(corrected2)[1:100,])
